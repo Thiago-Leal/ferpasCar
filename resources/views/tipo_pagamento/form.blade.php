3d3f9ca4fef38b50a44805054b1f56f72be86bab
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-22 23:48:41 +0800

    Delete resources directory

--- resources/views/tipo_pagamento/form.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="row">
-    <div class="col-md-6">
-        <div class="form-group {!! $errors->has('name') ? 'has-error' : '' !!}">
-            {!! Form::label('name', 'Nome') !!}
-            {!! Form::text('name', null, ['class'=>'form-control']) !!}
-            {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
-        </div>
-    </div>
-</div>
